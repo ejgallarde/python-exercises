2424412175ca21a2079db569127d732dabf41867
Earl John Gallarde <earl.john.gallarde@oracle.com>
2023-03-25 11:31:28 +0800

    Chapter 3 Exercises

---
+++ src/3_flow_control/ex3.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+# Name:         ex3.py
+# Author:       Earl John Gallarde
+# Version:      1.0
+# Description:  Use this as a template for globbing.py
+
+"""
+    Template for globbing exercise
+"""
---
+++ src/3_flow_control/globbing.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+# Name:         globbing.py
+# Author:       Earl John Gallarde
+# Version:      1.0
+# Description:  This program displays the files in your home directory, with their size.
+
+"""
+     Objective:
+    To use the flow control structures of Python and to gain familiarity in coding based on indentation!
+
+    Introduce learners to a couple of modules from the Python standard library.
+"""
+import sys
+import os
+
+
+sys.exit(0)
