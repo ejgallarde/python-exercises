d9df8518e36bc9ad546fe79ff02b25f209f1ee08
Earl John Gallarde <earl.john.gallarde@oracle.com>
2023-03-30 16:41:00 +0800

    Updating Chapter 4 Exercises

--- src/4_string_handling/greek.py
+++ src/4_string_handling/greek.py
@@ -2,8 +2,24 @@
 # Name:         greek.py
 # Author:       Earl John Gallarde
 # Version:      1.0
-# Description:  
+# Description:  Program that plays with unicode characters, particularly the Greek alphabet.
 
 """
-    DocString:
+    This exercise demonstrates the use of f strings and techniques in formatting
 """
+import sys
+
+greek = ['Alpha', 'Beta', 'Gamma', 'Delta', 'Epsilon', 'Zeta',
+         'Eta', 'Theta', 'Iota', 'Kappa', 'Lambda', 'Mu', 'Nu',
+         'Xi', 'Omicron', 'Pi', 'Rho', 'Sigma final', 'Sigma', 'Tau', 'Upsilon',
+         'Phi', 'Chi', 'Psi', 'Omega']
+
+try:
+    pos = 0x03b1
+    for name in greek:
+        print(f"{pos:#x} {name:<12s} : {chr(pos)} {chr(pos).upper()}")
+        pos += 1
+except UnicodeError as err:
+    print('unknown', err)
+
+sys.exit(0)
