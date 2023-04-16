040a0dc9788fd7bc98cf60d96b7a7b191d6b99d9
Earl John Gallarde <earl.john.gallarde@oracle.com>
2023-04-17 00:16:07 +0800

    Updating Chapter 5 Exercises

---
+++ src/5_collections/shareprices.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+# Name:         shareprices.py
+# Author:       Earl John Gallarde
+# Version:      1.0
+# Description:  This program calculates random share prices for some fictional companies
+#               and displays them all continually, with a two-second delay between each display.
+
+"""
+    DocString:
+"""
+import time
+import random
+
+share_prices = {'Global Motors': 50,
+                'Big Blue Inc.': 50,
+                'Gates Software': 50,
+                'Banana Computers': 50
+                }
+
+while True:
+    # We use the items() method to get a view object of the key-value pairs in the dictionary
+    for share, price in share_prices.items():
+        price = max(1.0, price * (1 + ((random.random() - 0.5)/0.5) * 0.05))
+        share_prices[share] = price
+        print("{:<18s}${:05.2f}".format(share, share_prices[share]))
+        print()
+        time.sleep(2)
